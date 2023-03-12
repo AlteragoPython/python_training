ad46babd8b52e73787b51f3569d49d724e912c62
Mikhail_Chizhov <mikhail.chizhov.test@gmail.com>
2023-03-12 22:03:37 +0300

    Add function major_and_minor_elem of HW1

--- HW1.py
+++ HW1.py
@@ -1,9 +1,12 @@
 from collections import Counter
-mylist = [2,2,1,1,1,2,2]
-counter_list = Counter(mylist)
-most_common = counter_list.most_common(1)
-least_common = counter_list.most_common()[:-2:-1]
-max_element = most_common[0][0]
-min_element = least_common[0][0]
-print(max_element,',',min_element)
 
+def major_and_minor_elem(self):
+    counter_list = Counter(mylist)
+    most_common = counter_list.most_common(1)
+    least_common = counter_list.most_common()[:-2:-1]
+    max_element = most_common[0][0]
+    min_element = least_common[0][0]
+    print (max_element,',', min_element)
+
+mylist = [2,2,1,1,1,2,2]
+major_and_minor_elem(mylist)
