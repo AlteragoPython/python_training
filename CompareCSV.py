4acd2871cd8f60f9ecf8b66a693bc02359f6bfa0
Mikhail_Chizhov <mikhail.chizhov.test@gmail.com>
2023-03-22 21:38:19 +0300

    update CompareCSV.py

--- CompareCSV.py
+++ CompareCSV.py
@@ -1,18 +1,19 @@
 import pandas as pd
-import numpy as np
 
-# load file1
-df1 = pd.read_csv('file1.csv')
+# Load the two CSV files into memory as pandas DataFrames
+df1 = pd.read_csv("file1.csv")
+df2 = pd.read_csv("file2.csv")
 
-# load file2
-df2 = pd.read_csv('file2.csv')
+# Compare the two DataFrames
+compare = df1.compare(df2)
 
-# compare values of two files
-if df1.equals(df2):
-    print("Values are the same")
-else:
-    # find diferens in two files
-    diff = np.where(df1 != df2)
-    print(f"Deferenses are find {len(diff[0])} field:")
-    for row, col in zip(diff[0], diff[1]):
-        print(f"Row {row}, column {col}: {df1.iloc[row, col]} != {df2.iloc[row, col]}")
+# Create a new DataFrame to store the rows that are different
+diff = pd.DataFrame(columns=df1.columns)
+
+# Iterate through the results of the compare method and add the rows that are different to the new DataFrame
+for row in compare.index:
+    if compare.loc[row].any():
+        diff = pd.concat([diff, df2.loc[row].to_frame().transpose()])
+
+# Save the new DataFrame to a new CSV file
+diff.to_csv("different_rows.csv", index=False)
