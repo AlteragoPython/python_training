800234af0b988c134d97c9c1c175be7830b650e6
Mikhail_Chizhov <mikhail.chizhov.test@gmail.com>
2023-03-29 13:44:26 +0300

    update HW3_1.py

---
+++ HW3_1.py
@@ -0,0 +1,24 @@
+def func(a, b):
+    return (a ** b) ** 2
+
+from functools import wraps
+
+def cache(func):
+    cache_dict = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        key = (args, tuple(kwargs.items()))
+        if key not in cache_dict:
+            cache_dict[key] = func(*args, **kwargs)
+        return cache_dict[key]
+
+    return wrapper
+
+
+cache_func = cache(func)
+some = 100, 200
+val_1 = cache_func(*some)
+val_2 = cache_func(*some)
+assert val_1 is val_2
+
