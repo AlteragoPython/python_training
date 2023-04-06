a9892f8cbaaaabf35a9b9a1eb41ccfab40aada5a
Mikhail_Chizhov <mikhail.chizhov.test@gmail.com>
2023-04-06 22:55:50 +0300

    update HW3_3.py

--- HW3_3.py
+++ HW3_3.py
@@ -1,10 +1,17 @@
-def custom_range(seq, start, stop=None, step=1):
-    if stop is None:
-        stop = start
-        start = seq[0]
-    index_start = seq.index(start)
-    if step > 0:
-        index_stop = len(seq) if stop is None else seq.index(stop)
-    else:
-        index_stop = seq.index(stop) - 1 if stop is not None else -1
-    return list(seq[index_start:index_stop:step])
+def custom_range(seq, *args):
+    start, stop, step = (None, None, 1)
+
+    if len(args) == 1:
+        stop = args[0]
+    elif len(args) == 2:
+        start, stop = args
+    elif len(args) == 3:
+        start, stop, step = args
+
+    if start is not None and start not in seq:
+        raise ValueError(f"{start} is not in sequence")
+
+    index = seq.index(start) if start is not None else 0
+    end_index = seq.index(stop) if stop is not None else len(seq)
+
+    return [seq[i] for i in range(index, end_index, step)]
