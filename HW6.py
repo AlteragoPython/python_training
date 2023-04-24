c27db8af5ac51dfa9ea363bbe959be001675c086
Mikhail_Chizhov <mikhail.chizhov.test@gmail.com>
2023-04-24 19:30:07 +0300

    update HW6.py

--- HW6.py
+++ HW6.py
@@ -13,3 +13,19 @@ d = [[1, 2], [3, 4]]
 for _ in merge_elems(a, b, c, d):
     print(_, end=' ')
 
+def map_like(fun, *elems):
+    for elem in elems:
+        try:
+            result = fun(elem)
+        except Exception as e:
+            result = f"{elem}: {str(e)}"
+        yield result
+
+a = [1, 2, 3]
+b = 6
+c = 'zhaba'
+d = True
+fun = lambda x: x[0]
+
+for _ in map_like(fun, a, b, c, d):
+    print(_)
