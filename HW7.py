6772396c3091dc5cc11818a9f5672410c4455e56
Mikhail_Chizhov <mikhail.chizhov.test@gmail.com>
2023-05-16 22:56:07 +0300

    add update HW7.py

--- HW7.py
+++ HW7.py
@@ -19,3 +19,32 @@ Base.metadata.create_all(engine)
 
 # Create the table
 Base.metadata.create_all(engine)
+
+
+# Create a new session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Add 3 films to the film table
+film1 = Film(id=1, title="Film1", director="Director1", release_year=2000)
+film2 = Film(id=2, title="Film2", director="Director2", release_year=2001)
+film3 = Film(id=3, title="Film3", director="Director3", release_year=2002)
+
+session.add(film1)
+session.add(film2)
+session.add(film3)
+session.commit()
+
+# Update 1 film
+film_to_update = session.query(Film).filter(Film.id == 1).first()
+film_to_update.title = "Updated Film1"
+session.commit()
+
+# Print data from table
+films = session.query(Film).all()
+for film in films:
+    print(f'ID: {film.id}, Title: {film.title}, Director: {film.director}, Release Year: {film.release_year}')
+
+# Delete all data from table
+session.query(Film).delete()
+session.commit()
